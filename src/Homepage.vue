ec435200b79c57de1472ea26c25f431814f5918d
Bogdan Calapod <bogdan.marius.calapod@outlook.com>
2017-01-04 19:33:14 +0200

    Start basic header layout

---
+++ src/Homepage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h2> Hello </h2>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/common/Header'
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+@import './common.scss';
+
+img {
+  height: 50px;
+}
+</style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,28 @@
+<!--
+  Header component 
+-->
+<template>
+  <section class="Header">
+    <div class="menu">
+      <img src="../../assets/img/badge_white.svg">
+      <div class="separator"></div>
+      <ul class="menu">
+        <li> about </li>
+        <li> bachelor </li>
+        <li> master </li>
+        <li> people </li>
+        <li> fb </li>
+        <li> mail </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+section.Header {
+  height: 100vh;
+  img {
+    height: 50px;
+  }
+}
+</style>

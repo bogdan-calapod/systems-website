da64f8e47693f5b114932fa1550201a06d442348
Bogdan Calapod <bogdan.marius.calapod@outlook.com>
2017-02-04 12:56:12 +0200

    Add basic server and build scripts

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Bogdan Calapod | Tech Lounge | 28 Jan 2017
+ *
+ * Website generator
+ *
+ * CLI interface for building a website using
+ * Harp, Sass and JS
+ *
+ */
+
+
---
+++ scripts/dev.js
@@ -0,0 +1,8 @@
+/**
+ * Bogdan Calapod | Tech Lounge | 28 Jan 2017
+ *
+ * Development start script
+ *
+ * Configures and starts the server and compile watcher
+ */
+

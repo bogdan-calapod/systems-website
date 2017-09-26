076185b03cd33947dad0b6bf309da7f4ab450898
Bogdan Marius Calapod <bogdan.calapod@codaintelligence.com>
2017-09-26 09:13:40 +0300

    Updated content

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 Systems Department Website
 ===
 
-TODO
+Install prerequisites:
+
+* Node v.6.9.5 (important!)
+
+To develop:
+
+* `npm run dev`
+
+To deploy:
+
+* `npm run build` annd deploy the `dist` folder
+
+For deployment - also set up a cronjob for `node index.js --no-rebuild`
+in the `_drive` folder
+
+TODO: Rewrite build system, make it more generic
--- _drive/index.js
+++ _drive/index.js
@@ -1,64 +1,58 @@
 /**
  * Entry point
  * -----------
- * 
+ *
  * This is the entry point of the application. Depending on parameters,
  * it runs certain operations and then regenerates the website
- * 
+ *
  * * `--all` : (optional) Run all steps
  * * `--download-files': Downloads Google Drive files
  * * `--download-sheets`: Download Google Drive spreadsheets as JSON
  * * `--rebuild-website`: Runs only the website rebuild script
- * 
- * If the `--no-rebuild` parameter is passed, the script will skip the 
+ *
+ * If the `--no-rebuild` parameter is passed, the script will skip the
  * website rebuild parameter
  */
 
 // Import modules
-let fs = require('fs');
-let Utility = require('./Utility.js');
-let Files = require('./Files.js');
-let Sheets = require('./Sheets.js');
+let fs = require('fs')
+let Utility = require('./Utility.js')
+let Files = require('./Files.js')
+let Sheets = require('./Sheets.js')
 
-console.log('Reading configuration file');
-let config = require('./config/config.json');
+console.log('Reading configuration file')
+let config = require('./config/config.json')
 
 // Process arguments and get environment
-let env = Utility.processArguments(process);
+let env = Utility.processArguments(process)
 
 // Initialize files processor 
-let files = new Files(config);
-let sheets = new Sheets(config); 
+let files = new Files(config)
+let sheets = new Sheets(config) 
 
-console.log(process.env.APPDATA);
+console.log(process.env.APPDATA)
 
 // Execute requested operation
-switch(env.operation) {
-  case '--download-files': 
-    files.download(env);
-    break;
-  
+switch (env.operation) {
+  case '--download-files':
+    files.download(env)
+    break
+
   case '--download-sheets':
-    sheets.download(env);
-    break;
-  
+    sheets.download(env)
+    break
+
   case '--rebuild-website':
-    Utility.rebuildWebsite(config.scripts.generate, env);
-    break;
-  
+    Utility.rebuildWebsite(config.scripts.generate, env)
+    break
+
   case '--all':
-    files.download(env);
-    sheets.download(env);
-    Utility.rebuildWebsite(config.scripts.generate, env);
-    break;
-  
+    files.download(env)
+    sheets.download(env)
+    Utility.rebuildWebsite(config.scripts.generate, env)
+    break
+
   default: 
-    console.error('Invalid operation: ', env.operation);
-    process.exit();
+    console.error('Invalid operation: ', env.operation)
+    process.exit()
 }
-
-
-
-
-
-

f12e3ab846cbefddc485fb80372d3eb1eef39683
Bogdan Marius Calapod <bogdan.marius.calapod@gmail.com>
2018-09-27 20:36:52 +0300

    Fix bachelor route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,42 +2,39 @@
  * Main routes index component
  */
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from "styled-components";
 
-import Home from 'routes/Home'
-import People from 'routes/People'
-import Bachelor from 'routes/Bachelor'
-import Master from 'routes/Master'
-import PhD from 'routes/PhD'
+import Home from "routes/Home";
+import People from "routes/People";
+import Bachelor from "routes/Bachelor";
+import Master from "routes/Master";
+import PhD from "routes/PhD";
 
 let theme = {
-  primary: '#F7931E'
-}
+  primary: "#F7931E"
+};
 
 class MainRouter extends Component {
-  render () {
+  render() {
     return (
       <ThemeProvider theme={theme}>
         <Router>
           <Switch>
-            <Route path='/people' component={People} />
-            <Route path='/bachelor' component={Bachelor} />
-            <Route path='/master' component={Master} />
-            <Route path='/phd' component={PhD} />
+            <Route path="/people" component={People} />
+            <Route path="/bachelor" component={Bachelor} />
+            <Route path="//bachelor" component={Bachelor} />
+            <Route path="/master" component={Master} />
+            <Route path="/phd" component={PhD} />
             <Route component={Home} />
           </Switch>
         </Router>
       </ThemeProvider>
-    )
+    );
   }
 }
 
-export default MainRouter
+export default MainRouter;

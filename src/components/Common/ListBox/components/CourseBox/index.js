6c24aec64f04db7ea9bc14667767ccc093810029
Bogdan Marius Calapod <bogdan.marius.calapod@gmail.com>
2018-10-03 14:53:18 +0300

    Use async await when making requests

--- src/components/Common/ListBox/components/CourseBox/index.js
+++ src/components/Common/ListBox/components/CourseBox/index.js
@@ -2,23 +2,23 @@
  * Course box display
  */
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import Button from 'components/Common/Button'
+import Button from "components/Common/Button";
 
-import HitListModal from '../HitListModal'
+import HitListModal from "../HitListModal";
 
 const propTypes = {
   data: PropTypes.object
-}
+};
 
 const Row = styled.div`
   display: flex;
   align-items: flex-start;
-  justify-content: ${props => props.left ? 'flex-start' : 'space-between'};
+  justify-content: ${props => (props.left ? "flex-start" : "space-between")};
 
   @media screen and (max-width: 900px) {
     flex-direction: column;
@@ -34,105 +34,114 @@ const Row = styled.div`
   button {
     margin: 0 10px;
   }
-`
+`;
 
 const Column = styled.div`
   display: flex;
   flex-direction: column;
 
-  max-width: ${props => props.main ? '500px' : 'none'};
+  max-width: ${props => (props.main ? "500px" : "none")};
   background-color: white;
   margin: 10px 0;
-`
+`;
 
 const Logo = styled.img`
   max-width: none;
   max-height: none;
   width: 150px;
   margin: 10px;
-`
+`;
 
 const Title = styled.h1`
   margin: 0;
   font-size: 21px;
-`
+`;
 
 const Para = styled.p`
   font-size: 14px;
   margin: 0;
-`
+`;
 
 class CourseBox extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       modalOpen: false
-    }
+    };
   }
 
   componentDidMount() {
-    let cleanedName = '/' + this.props.data.abbreviation.toLowerCase()
+    let cleanedName = "/" + this.props.data.abbreviation.toLowerCase();
+
+    console.log(cleanedName);
 
     if (window.location.href.toLowerCase().includes(cleanedName)) {
-      this.setState({...this.state, modalOpen: true})
+      this.setState({ ...this.state, modalOpen: true });
     }
   }
 
   toggleModal = _ => {
-    this.setState({modalOpen: !this.state.modalOpen})
-  }
+    this.setState({ modalOpen: !this.state.modalOpen });
+  };
 
-  getButtons () {
-    let buttons = []
+  getButtons() {
+    let buttons = [];
 
     if (this.props.data.presentation) {
       buttons.push(
         <a href={this.props.data.presentation} key={0}>
-          <Button>
-            Course
-          </Button>
-        </a>)
+          <Button>Course</Button>
+        </a>
+      );
     }
 
     if (this.props.data.hitlist && this.props.data.hitlist.length > 0) {
       buttons.push(
         <div onClick={this.toggleModal}>
-          <Button key={1}>
-            Best students
-          </Button>
+          <Button key={1}>Best students</Button>
         </div>
-      )
+      );
     }
 
-    return buttons
+    return buttons;
   }
 
-  render () {
-    let {name, description, abbreviation, announcements, hitlist} = this.props.data
-    let {modalOpen} = this.state
+  render() {
+    let {
+      name,
+      description,
+      abbreviation,
+      announcements,
+      hitlist
+    } = this.props.data;
+    let { modalOpen } = this.state;
 
     return (
       <Column main>
         <Row left>
-          <Logo src={'/courses/' + abbreviation.toLowerCase() + '.svg'} alt='' />
+          <Logo
+            src={"/courses/" + abbreviation.toLowerCase() + ".svg"}
+            alt=""
+          />
           <Column>
             <Title>{name}</Title>
             <Para>{description}</Para>
           </Column>
         </Row>
-        <Row>
-          {this.getButtons()}
-        </Row>
-        <HitListModal visible={modalOpen} onClose={this.toggleModal}
+        <Row>{this.getButtons()}</Row>
+        <HitListModal
+          visible={modalOpen}
+          onClose={this.toggleModal}
           abbreviation={abbreviation}
           data={hitlist}
-          announcements={announcements} />
+          announcements={announcements}
+        />
       </Column>
-    )
+    );
   }
 }
 
-CourseBox.propTypes = propTypes
+CourseBox.propTypes = propTypes;
 
-export default CourseBox
+export default CourseBox;
--- src/routes/Bachelor/index.js
+++ src/routes/Bachelor/index.js
@@ -2,46 +2,59 @@
  * Home Routing Component
  */
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Container from 'components/Bachelor'
+import Container from "components/Bachelor";
 
-import request from 'axios'
+import request from "axios";
 
 class Bachelor extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       projects: [],
       courses: [],
       people: []
+    };
+  }
+
+  async componentDidMount() {
+    let projects, courses, people, collaborators, date, url;
+
+    try {
+      projects = await request.get("/api/projects/");
+      courses = await request.get("/api/courses/");
+      people = await request.get("/api/people/");
+      collaborators = await request.get("/api/collaborators/");
+
+      date = collaborators.data[0].bachelorDate;
+      url = collaborators.data[0].bachelorURL;
+    } catch (e) {
+      console.warn("Error making requests");
+      return;
     }
+
+    this.setState({
+      projects: projects.data.filter(x => x.type === "bachelor"),
+      courses: courses.data.filter(x => x.type === "bachelor"),
+      people: people.data,
+      date,
+      url
+    });
   }
 
-  componentWillMount () {
-    request.get('/api/projects/')
-      .then(x => this.setState({...this.state, projects: x.data.filter(x => x.type === 'bachelor')}))
-
-    request.get('/api/courses/')
-      .then(x => this.setState({...this.state, courses: x.data.filter(x => x.type === 'bachelor')}))
-
-    request.get('/api/people/')
-      .then(x => this.setState({...this.state, people: x.data}))
-
-    request.get('/api/collaborators/')
-      .then(x => this.setState({...this.state, date: x.data[0].bachelorDate, url: x.data[0].bachelorURL}))
-  }
-
-  render () {
+  render() {
     return (
-      <Container projects={this.state.projects}
+      <Container
+        projects={this.state.projects}
         courses={this.state.courses}
         date={this.state.date}
         url={this.state.url}
-        people={this.state.people} />
-    )
+        people={this.state.people}
+      />
+    );
   }
 }
 
-export default Bachelor
+export default Bachelor;

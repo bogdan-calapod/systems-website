8b214092039d0d60e5a768748ba6f19424177d92
Bogdan Marius Calapod <bogdan.marius.calapod@gmail.com>
2018-10-15 15:21:08 +0300

    Update master announcement

--- src/components/Master/index.js
+++ src/components/Master/index.js
@@ -2,29 +2,29 @@
  * Bachelor page presentational component
  */
 
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import Header from 'components/Common/Header'
-import People from 'components/People'
-import SectionTitle from 'components/Common/SectionTitle'
-import ListBox from 'components/Common/ListBox'
-import Button from 'components/Common/Button'
+import Header from "components/Common/Header";
+import People from "components/People";
+import SectionTitle from "components/Common/SectionTitle";
+import ListBox from "components/Common/ListBox";
+import Button from "components/Common/Button";
 
-import sric from './sric.svg'
-import styled from 'styled-components'
+import sric from "./sric.svg";
+import styled from "styled-components";
 
 const propTypes = {
   projects: PropTypes.array,
   courses: PropTypes.array,
   people: PropTypes.array
-}
+};
 
 const defaultProps = {
   projects: [],
   courses: [],
   people: []
-}
+};
 
 const Container = styled.div`
   background-color: white;
@@ -49,7 +49,7 @@ const Container = styled.div`
       height: 50px;
     }
   }
-`
+`;
 
 const Lists = styled.div`
   display: flex;
@@ -59,11 +59,11 @@ const Lists = styled.div`
   p {
     max-width: 400px;
   }
-`
+`;
 
 class Master extends Component {
-  render () {
-    let { projects, courses, people } = this.props
+  render() {
+    let { projects, courses, people } = this.props;
 
     return (
       <div>
@@ -72,49 +72,48 @@ class Master extends Component {
           <SectionTitle> Master </SectionTitle>
           <p>
             <a
-              href='http://sric.cs.pub.ro'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="http://sric.cs.pub.ro"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <img src={sric} alt='' />
+              <img src={sric} alt="" />
               Be sure to check out our Master program at sric.cs.pub.ro!
             </a>
           </p>
           <p>
-            Delve in-depth in the upcoming information infrastructures, and learn to tame the wildest networks. Prepare to confront attacks with dedicated techniques and equipment. Get a better grip on mobile systems, and secure the biggest and the smallest devices. Find your path in the research community of applied and fundamental ICT scholars, and join dedicated mentors and instructors in R&D projects of your choice.
+            Delve in-depth in the upcoming information infrastructures, and
+            learn to tame the wildest networks. Prepare to confront attacks with
+            dedicated techniques and equipment. Get a better grip on mobile
+            systems, and secure the biggest and the smallest devices. Find your
+            path in the research community of applied and fundamental ICT
+            scholars, and join dedicated mentors and instructors in R&D projects
+            of your choice.
           </p>
           <Lists>
-            <ListBox title='Courses' type='course' data={courses} />
-            <ListBox title='Projects' type='project' data={projects} showFilter>
-              <p>
-                For SRIC 1, the research reports should be uploaded until June 9th
-                2018.
-                Research report defence will take place on June 14th 2018, in
-                PR706, from 9:00.
-              </p><p>
-                For SRIC 2, the research reports should be uploaded until June 2nd
-                2018.
-                Research report defence will take place on June 7th 2018, in
-                PR706, from 9:00.
-              </p><p>
-                For SAS 1, the research reports should be uploaded until June 7th
-                2018.
-                Research report defence will take place on June 13th 2018, in
-                PR706, from 9:00.
-              </p>
-              <a href='https://docs.google.com/forms/d/e/1FAIpQLSePOIhrt_A06ZOyNX0xnT_2_v0JGdPFiCn6wOWn3XHng-zUGQ/viewform'>
-                <Button> Register here </Button>
+            <ListBox title="Courses" type="course" data={courses} />
+            <ListBox title="Projects" type="project" data={projects} showFilter>
+              Research projects should be registered until November 1st.
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSePOIhrt_A06ZOyNX0xnT_2_v0JGdPFiCn6wOWn3XHng-zUGQ/viewform">
+                <Button>Register your presentation here</Button>
               </a>
+              <p>All research reports should be sumbited by Februrary 4th.</p>
+              <p>Research report defence will take place: </p>
+              <ul>
+                <li>SAS1: Februrary 6th, 9:00, PR705</li>
+                <li>SAS2: Februrary 7th, 9:00, PR705</li>
+                <li>SRIC1: Februrary 6th, 9:00, PR706 </li>
+                <li>SRIC2: Februrary 7th, 9:00, PR706</li>
+              </ul>
             </ListBox>
           </Lists>
         </Container>
         <People data={people} />
       </div>
-    )
+    );
   }
 }
 
-Master.propTypes = propTypes
-Master.defaultProps = defaultProps
+Master.propTypes = propTypes;
+Master.defaultProps = defaultProps;
 
-export default Master
+export default Master;
